5b823fa32217c92d85248fc0b410d302ccc52946
kabirHossain <kabir20304@gmail.com>
2026-06-07 17:06:57 +0600

    home page upper section is done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Keen Keeper</h1>
+      <div className="upper container mx-auto flex flex-col items-center justify-between py-10 gap-2">
+        <h2 className='text-xl font-bold text-black'>Friends to keep close in your life</h2>
+        <p className='mt-4 text-gray-600 text-xs text-center'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br/>
+relationships that matter most.</p>
+        <button className="mt-6 text-white font-bold btn bg-[#244D3F]"> + Add Friend</button>
+      </div>
     </div>
   );
 }
